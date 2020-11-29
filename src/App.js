f5aa7cb866bf211912dbc85cdd212c1005ceb62b
unknown <jreidmke@gmail.com>
2020-11-29 12:06:34 -0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Be sure to show a list of colors at /colors */}
+
+      {/* Then there should be routing for a form that allows you to create a new color */}
+
+      {/* This means the color pages themselves will have to be abstracted. I could actually just do a css thing. Or no, forget that. I would do inline styling with a prop for background color. Cool */}
+    </div>
+  );
+}
+
+export default App;
