2b30065277d784b42e988dae213d0c64242e668d
unknown <jreidmke@gmail.com>
2020-12-02 09:20:40 -0600

    new styling and return button

--- src/ColorPage.js
+++ src/ColorPage.js
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const ColorPage = ({colors}) => {
     const {name} = useParams();
     return(
-        <div style={{backgroundColor:name}}>
+        <div style={{backgroundColor:name, height:'100em'}}>
             <h1 style={{color:'white'}}>{name}</h1>
+            <h3><Link to={'/colors'}>Return Home</Link></h3>
         </div>
     )
 }
